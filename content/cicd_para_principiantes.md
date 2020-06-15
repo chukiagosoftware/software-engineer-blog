07c2d0cd973066289b824b2cfd16e608c20181a7
ericdanielarellano <elericarellano@gmail.com>
2020-06-15 01:49:09 -0700

    initial commit

---
+++ content/cicd_para_principiantes.md
@@ -0,0 +1,9 @@
+Title: CICD para Principiantes
+Date: December 28, 2019
+Category: DevOps
+
+# Que es CICD?
+
+*En la vida real*
+
+
---
+++ pelicanconf.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Eric Arellano'
+SITENAME = 'Software Engineering Blog'
+SITEURL = 'www.ericarellano.tech'
+THEME = 'themes/pelican-blueidea'
+PATH = 'content'
+LOCALE = 'en_us'
+
+TIMEZONE = 'America/New_York'
+DEFAULT_DATE = 'fs'
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pagina Siete (Bolivia)', 'http://www.paginasiete.bo'),
+         ('Fundación Solón', 'https://www.fundacionsolon.org'),
+         ('Rimay Pampa', 'http://www.rimaypampa.com'))
+
+# Social widget
+SOCIAL = [('Github', 'http://www.github.com/ericdanielarellano')]
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
+
+# BlueIdea
+
+GITHUB_URL='http://www.github.com/ericdanielarellano'
