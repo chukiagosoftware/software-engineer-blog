f6ed8f592f3900e4bd3028b7ea36b30025cbc7cc
Eric Arellano <ericarellano@github.com>
2022-09-18 19:54:08 -0500

    Install stork via rust toolchain

--- build_stork_search.sh
+++ build_stork_search.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-wget -O stork https://files.stork-search.net/releases/v1.5.0/stork-ubuntu-20-04
-chmod +x stork
+rustup toolchain install stable
+cargo install stork-search --locked
