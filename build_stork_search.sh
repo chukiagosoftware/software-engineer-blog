458addbae2fe0af00b30ee452fc1ac8ee48bbe8e
Eric Arellano <ericarellano@github.com>
2022-09-18 19:29:14 -0500

    Move stork to /usr/local/bin

--- build_stork_search.sh
+++ build_stork_search.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 wget https://files.stork-search.net/releases/v1.5.0/stork-ubuntu-20-04
-chmod +x stork-ubuntu-20-04
+mv stork-ubuntu-20-04 /usr/local/bin/stork
+chmod +x /usr/local/bin/stork
