5424c182398102e6dcb78ed60b454f4e4091c21d
Eric Arellano <ericarellano@github.com>
2022-09-18 19:34:59 -0500

    Save stork as just stork

--- build_stork_search.sh
+++ build_stork_search.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-wget https://files.stork-search.net/releases/v1.5.0/stork-ubuntu-20-04
-mv stork-ubuntu-20-04 /usr/local/bin/stork
-chmod +x /usr/local/bin/stork
+wget -O stork https://files.stork-search.net/releases/v1.5.0/stork-ubuntu-20-04
+chmod +x stork
